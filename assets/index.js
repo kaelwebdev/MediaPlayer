e44d40194e0355660e32c8986c94ea52ac84c403
kael1706 <cdcp-1795@hotmail.com>
2020-12-29 11:29:50 -0500

    adding new pause plugin

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,13 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
-const mP = new MediaPlayer({ item: video,  plugins: [new AutoPlay]});
+const mP = new MediaPlayer({
+    item: video,
+    plugins: [new AutoPlay(),
+        new AutoPause()
+    ]});
 
 const btn1 = document.querySelector('#playPause');
 const btn2 = document.querySelector('#muteUnmute');
