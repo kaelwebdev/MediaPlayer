b700ad17528dfd9aab08b1c3775280cb0ba95f7d
kael1706 <cdcp-1795@hotmail.com>
2021-01-02 10:47:51 -0500

    ad plugin added

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,17 +1,20 @@
 interface ConfigObj {
-    item: HTMLMediaElement,
-    plugins?: Array<any>
+    item: HTMLMediaElement;
+    plugins?: Array<any>;
 }
 
 class MediaPlayer {
     media: HTMLMediaElement;
     plugins: Array<any>;
+    container: HTMLElement;
 
     constructor(config: ConfigObj) {
         this.media = config.item;
         this.plugins = config.plugins || [];
+        this.initPlayer();
         this.initPlugins();
     }
+
     private initPlugins() {
         /**
          * Description: is a function that initializes
@@ -21,6 +24,13 @@ class MediaPlayer {
         this.plugins.forEach(plugin => {
             plugin.run(this);
         });
+    }
+
+    initPlayer() {
+        this.container = document.createElement('div');
+        this.container.style.position = 'relative';
+        this.media.parentNode.insertBefore(this.container, this.media);
+        this.container.appendChild(this.media);
     }
 
     play() {
